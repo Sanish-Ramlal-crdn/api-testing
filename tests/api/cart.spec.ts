501cc8b6da83f0207fc6b384123bbd9bbfb178d2
Sanish-Ramlal-crdn <Sanish.Ramlal@dayforce.com>
2025-06-24 12:16:09 +0400

    Adding test to delete item from cart

--- tests/api/cart.spec.ts
+++ tests/api/cart.spec.ts
@@ -37,7 +37,7 @@ test("POST Add product to cart", async ({ request }) => {
   }
 });
 
-test.only("GET cart details", async ({ request }) => {
+test("GET cart details", async ({ request }) => {
   const cartId = await createCart(request, path);
 
   const startTime = performance.now();
@@ -51,6 +51,41 @@ test.only("GET cart details", async ({ request }) => {
       "Cart with id " + cartId + " fetched successfully - Test Passed"
     );
 
+    checkResponseTime(startTime, endTime);
+  } catch (error) {
+    if (res) {
+      const responseBody = await res.json();
+      console.log("Error response:", responseBody, " - Test failed");
+    }
+    throw error;
+  }
+});
+
+test("DELETE item from cart", async ({ request }) => {
+  const cartId = await createCart(request, path);
+  const productId = await getProductId(request, productPath);
+
+  const startTime = performance.now();
+  let res;
+  try {
+    res = await request.post(`${path}/${cartId}`, {
+      data: {
+        product_id: productId,
+        quantity: 1,
+      },
+    });
+    const startTime = performance.now();
+    res = await request.delete(`${path}/${cartId}/product/${productId}`);
+    const endTime = performance.now();
+
+    expect(res.status()).toBe(204);
+    console.log(
+      "Product with id " +
+        productId +
+        " removed from cart " +
+        cartId +
+        " - Test Passed"
+    );
     checkResponseTime(startTime, endTime);
   } catch (error) {
     if (res) {
