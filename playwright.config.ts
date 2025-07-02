12f0239e31b683ee2731a160739d7c4ada1b272d
Sanish-Ramlal-crdn <Sanish.Ramlal@dayforce.com>
2025-07-02 08:08:55 +0400

    update gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html"],
+    ["html", { outputFile: "playwright-report/index.html" }],
     ["monocart-reporter", { outputFile: "monocart-report/index.html" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
